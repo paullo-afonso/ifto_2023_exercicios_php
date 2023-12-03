0f4bfaffc2fa854ad1a303455d39bf5d8869f06d
Paulo Afonso <paulo@pauloafonso.dev>
2023-12-03 18:03:54 -0300

    concatenando strings

--- concatenacao-de-strings.php
+++ concatenacao-de-strings.php
@@ -1,16 +1,25 @@
 <?php
 
-// $nome = "Paulo";
-// $sobrenome = "Afonso";
-// $nomeCompleto = $nome . " " . $sobrenome;
+$nome = "Paulo";
+$sobrenome = "Afonso";
+$nomeCompleto = $nome . " " . $sobrenome;
 
-// echo $nomeCompleto;
+echo $nomeCompleto . "<hr>";
 
 $profissao = "Professor ";
-$nome = "Paulo ";
-$sobrenome = "Afonso";
+$nome2 = "Paulo ";
+$sobrenome2 = "Afonso";
 $usuario = $profissao;
-$usuario .= $nome;
-$usuario.= $sobrenome;
+$usuario .= $nome2;
+$usuario.= $sobrenome2;
 
-echo $usuario;
+echo $usuario . "<hr>";
+
+$nome3 = "Maria";
+$idade = 30;
+$mensagem = "Olá, " . $nome3 . "! Você tem " . $idade . " anos. <hr>";
+
+$alternativa = "Olá, $nome3! Você tem $idade anos";
+echo $mensagem;
+
+echo $alternativa;
