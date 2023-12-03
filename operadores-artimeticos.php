6950d7a3270558b75efedd43fa0cc2571b459123
Paulo Afonso <paulo@pauloafonso.dev>
2023-12-03 17:41:07 -0300

    treino com operadores aritméticos

---
+++ operadores-artimeticos.php
@@ -0,0 +1,36 @@
+<?php
+
+
+$valor1 = 10;
+$valor2 = 5;
+
+$soma = $valor1 + $valor2; // Soma
+$subtracao = $valor1 - $valor2; // Subtração
+$multiplicacao = $valor1 * $valor2; // Multiplicação
+$potencia = $valor1 ** $valor2; // Potenciação
+$divisao = $valor1 / $valor2; // Divisão
+$resto = $valor1 % $valor2; // Módulo (resto da divisão)
+
+echo "Soma: " . $valor1 . ' + ' . $valor2 . ' = ' . $soma . "<br>"; //saída 15
+echo "Subtração: " . $valor1 . ' - ' . $valor2 . ' = ' . $subtracao . "<br>"; // saída
+echo "Multiplicação: " . $valor1 . ' * ' . $valor2 . ' = ' . $multiplicacao . "<br>"; // saída 50
+echo "Divisão: " . $valor1 . ' / ' . $valor2 . ' = ' . $divisao . "<br>"; // saída 2
+echo "Resto da Divisão: " . $valor1 . ' % ' . $valor2 . ' = ' . $resto . "<br>"; // saída 0
+echo "Potenciação: " . $valor1 . ' ** ' . $valor2 . ' = ' . $potencia . "<br>"; // saída 100000
+
+$numero1 = 5;
+$numero2 = 5;
+$numero1++; // incremento em 1
+$numero2--; // decremento em 1
+
+echo "Número após incremento: " . '5' . '++ ' . '= ' . $numero1 . "<br>"; // saida 5
+echo "Número após decremento: " . '5' . '-- ' . '= ' . $numero2 . "<br>"; // saida 4
+
+$numero3 = 5;
+$numero3 += 5; // aumenta o valor de $numero3 em 5, logo passa a ser 10
+
+echo "Número após aumento e resultado: " . '5' . '+= ' . 'passou a ser ' . $numero3; // saida 10
+
+
+
+
