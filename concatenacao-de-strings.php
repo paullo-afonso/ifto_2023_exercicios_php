9aeed930f300618d493e820d1fbf86574fd69b4e
Paulo Afonso <paulo@pauloafonso.dev>
2023-12-03 17:55:28 -0300

    concatenando nome sobrenome e profissao

---
+++ concatenacao-de-strings.php
@@ -0,0 +1,16 @@
+<?php
+
+// $nome = "Paulo";
+// $sobrenome = "Afonso";
+// $nomeCompleto = $nome . " " . $sobrenome;
+
+// echo $nomeCompleto;
+
+$profissao = "Professor ";
+$nome = "Paulo ";
+$sobrenome = "Afonso";
+$usuario = $profissao;
+$usuario .= $nome;
+$usuario.= $sobrenome;
+
+echo $usuario;
