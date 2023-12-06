2acca82a5a591ef86b7e2bd279ebaf0db56fa08a
Paulo Afonso <paulo@pauloafonso.dev>
2023-12-06 00:17:16 -0300

    revisando constantes

---
+++ constantes.php
@@ -0,0 +1,10 @@
+<?php
+
+define('NOME_DA_CONSTANTE', 'valor');
+
+define('PI', 3.14159);
+define('NOME_DO_SITE', 'Meu Site');
+
+echo "O valor de PI é " . PI . "<br>";
+echo "Bem-vindo ao site " . NOME_DO_SITE;
+
