d810cd69b726c319230e7a24390c5511785cafe0
Paulo Afonso <paulo@pauloafonso.dev>
2023-12-05 22:53:53 -0300

    treinando while e do-while

---
+++ laco-while-e-do-while.php
@@ -0,0 +1,17 @@
+<?php
+
+// // while
+
+// $contador = 1;
+// while ($contador <= 5) {
+//     echo "Número: " . $contador . "<br>";
+//     $contador++;
+// }
+
+// do while
+
+$contador = 1;
+do {
+    echo "Número: " . $contador . "<br>";
+    $contador++;
+} while ($contador <= 5);
